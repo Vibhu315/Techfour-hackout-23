c8457731b75c5ce05d9b0cb245a567462b9178d3
Jhamtanidev <jhamtanidev2004@gmail.com>
2023-08-26 16:24:35 +0530

    3D mdel page sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import "./Navbar.css"
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
-          ReactApp
-        </Link>
-        <ul className="nav-menu">
-          <li className="nav-item">
-            <Link to="/" className="nav-links">
-              Home
-            </Link>
-            <Link to="/3dmodel" className="nav-links">
-              3D model
-            </Link>
-          </li>
-          {/* Add more menu items here */}
-        </ul>
-      </div>
+      
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/3dmodel" className="nav-link">
+            3D Model
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/services" className="nav-link">
+            Services
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/contact" className="nav-link">
+            Contact
+          </Link>
+        </li>
+     
     </nav>
   );
 }
--- src/components/d_model.js
+++ src/components/d_model.js
@@ -1,8 +1,72 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
+// import * as THREE from 'three';
+
+// function ThreeJSBridge() {
+//     useEffect(() => {
+//     // Initialize scene, camera, and renderer
+//     const scene = new THREE.Scene();
+//     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+//     const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('canvas') });
+//     renderer.setSize(window.innerWidth, window.innerHeight);
+
+//     // Create a bridge-like structure
+//     const bridge = new THREE.Object3D();
+
+//     // Create bridge pillars
+//     const pillarGeometry = new THREE.BoxGeometry(0.5, 3, 0.5);
+//     const pillarMaterial = new THREE.MeshBasicMaterial({ color: 0x808080 });
+//     const pillar1 = new THREE.Mesh(pillarGeometry, pillarMaterial);
+//     pillar1.position.set(-2, 1.5, 0);
+//     bridge.add(pillar1);
+
+//     const pillar2 = new THREE.Mesh(pillarGeometry, pillarMaterial);
+//     pillar2.position.set(2, 1.5, 0);
+//     bridge.add(pillar2);
+
+//     // Create bridge deck
+//     const deckGeometry = new THREE.BoxGeometry(5, 0.2, 1);
+//     const deckMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+//     const deck = new THREE.Mesh(deckGeometry, deckMaterial);
+//     deck.position.set(0, 2.5, 0);
+//     bridge.add(deck);
+
+//     scene.add(bridge);
+
+//     // Position the camera
+//     camera.position.z = 8;
+
+//     // Render loop
+//     const animate = () => {
+//       requestAnimationFrame(animate);
+
+//       // Rotate the bridge
+//       bridge.rotation.y += 0.005;
+
+//       renderer.render(scene, camera);
+//     };
+//     animate();
+//   }, []);
+
+//   return <canvas id="canvas"></canvas>;
+// }
+
+// export default ThreeJSBridge;
+
+
+
+import React, { useState, useEffect } from 'react';
 import * as THREE from 'three';
 
-function ThreeJSBridge(e) {
-    useEffect(() => {
+function ThreeJSBridge() {
+    const [isSidebarExpanded, setIsSidebarExpanded] = useState(false);
+  const [positionX, setPositionX] = useState(0);
+  const [positionY, setPositionY] = useState(0);
+  const [positionZ, setPositionZ] = useState(0);
+  const [width, setWidth] = useState(5);
+  const [height, setHeight] = useState(3);
+  const [length, setLength] = useState(1);
+
+  useEffect(() => {
     // Initialize scene, camera, and renderer
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -13,21 +77,21 @@ function ThreeJSBridge(e) {
     const bridge = new THREE.Object3D();
 
     // Create bridge pillars
-    const pillarGeometry = new THREE.BoxGeometry(0.5, 3, 0.5);
+    const pillarGeometry = new THREE.BoxGeometry(0.5, height, 0.5);
     const pillarMaterial = new THREE.MeshBasicMaterial({ color: 0x808080 });
     const pillar1 = new THREE.Mesh(pillarGeometry, pillarMaterial);
-    pillar1.position.set(-2, 1.5, 0);
+    pillar1.position.set(positionX - 2, positionY + height / 2, positionZ);
     bridge.add(pillar1);
 
     const pillar2 = new THREE.Mesh(pillarGeometry, pillarMaterial);
-    pillar2.position.set(2, 1.5, 0);
+    pillar2.position.set(positionX + 2, positionY + height / 2, positionZ);
     bridge.add(pillar2);
 
     // Create bridge deck
-    const deckGeometry = new THREE.BoxGeometry(5, 0.2, 1);
+    const deckGeometry = new THREE.BoxGeometry(width, 0.2, length);
     const deckMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const deck = new THREE.Mesh(deckGeometry, deckMaterial);
-    deck.position.set(0, 2.5, 0);
+    deck.position.set(positionX, positionY + height, positionZ);
     bridge.add(deck);
 
     scene.add(bridge);
@@ -45,9 +109,105 @@ function ThreeJSBridge(e) {
       renderer.render(scene, camera);
     };
     animate();
-  }, []);
+  }, [positionX, positionY, positionZ, width, height, length]);
 
-  return <canvas id="canvas"></canvas>;
+  const sidebarStyles = {
+    position: 'fixed',
+    top: 75,
+    left: isSidebarExpanded? 0 : '-250px',
+    zIndex: 1,
+    width: '250px',
+    padding: '20px',
+    background: 'linear-gradient(to bottom, #3498db, #8E44AD)',
+    color: 'white',
+    transition: 'left 0.3s ease-in-out',
+    overflowY: 'auto',
+    height: '100vh',
+  };
+
+//   const canvasStyles = {
+//     flex: 1,
+//     marginLeft: sidebarExpanded ? '250px' : '0',
+//     transition: 'margin-left 0.3s ease-in-out',
+//   };
+  
+
+  const canvasStyles = {
+    // ... (other styles)
+    marginLeft: isSidebarExpanded ? '250px' : '0', // Corrected variable name
+  };
+  return (
+    <div style={{ display: 'flex', overflow: 'hidden' }}>
+      <div
+        id="sidebar"
+        style={sidebarStyles}
+        onMouseEnter={() => setIsSidebarExpanded(true)}
+        onMouseLeave={() => setIsSidebarExpanded(false)}
+      >
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '19px' }}>
+        <label htmlFor="positionX" style={{ display: 'flex', flexDirection: 'column', marginTop: '15px' }}>Position X:</label>
+        <input
+          type="number"
+          id="positionX"
+          placeholder="Enter X position"
+          value={positionX}
+          onChange={(e) => setPositionX(parseFloat(e.target.value))}
+        />
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '19px' }}>
+        <label htmlFor="positionY" style={{ display: 'flex', flexDirection: 'column', marginTop: '15px' }}>Position Y:</label>
+        <input
+          type="number"
+          id="positionY"
+          placeholder="Enter Y position"
+          value={positionY}
+          onChange={(e) => setPositionY(parseFloat(e.target.value))}
+        />
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '19px' }}>
+        <label htmlFor="positionZ" style={{ display: 'flex', flexDirection: 'column', marginTop: '15px' }}>Position Z:</label>
+        <input
+          type="number"
+          id="positionZ"
+          placeholder="Enter Z position"
+          value={positionZ}
+          onChange={(e) => setPositionZ(parseFloat(e.target.value))}
+        />
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '19px' }}>
+        <label htmlFor="width" style={{ display: 'flex', flexDirection: 'column', marginTop: '15px' }} >Width:</label>
+        <input
+          type="number"
+          id="width"
+          placeholder="Enter width"
+          value={width}
+          onChange={(e) => setWidth(parseFloat(e.target.value))}
+        />
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '19px' }}>
+        <label htmlFor="height" style={{ display: 'flex', flexDirection: 'column', marginTop: '15px' }}>Height:</label>
+        <input
+          type="number"
+          id="height"
+          placeholder="Enter height"
+          value={height}
+          onChange={(e) => setHeight(parseFloat(e.target.value))}
+        />
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '19px' }}>
+        <label htmlFor="length" style={{ display: 'flex', flexDirection: 'column', marginTop: '15px' }}>Length:</label>
+        <input
+          type="number"
+          id="length"
+          placeholder="Enter length"
+          value={length}
+          onChange={(e) => setLength(parseFloat(e.target.value))}
+        />
+      </div>
+    </div>
+    <canvas id="canvas" style={{ flex: '1' }}></canvas>
+  </div>
+  );
 }
 
 export default ThreeJSBridge;
