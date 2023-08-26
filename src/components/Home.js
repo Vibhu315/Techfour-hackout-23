280aacb899049bd8a263b2f678b665dd127d185f
Jhamtanidev <jhamtanidev2004@gmail.com>
2023-08-26 16:51:18 +0530

    ph2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,33 @@
 import React from 'react';
-
+// import image from "/public"
 function Home() {
+  const containerStyle = {
+    backgroundColor: '#E6F7FF',
+    padding: '20px',
+    borderRadius: '10px',
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+    textAlign: 'center',
+   
+  };
+
+  const headingStyle = {
+    color: '#FFFFFF',
+    fontSize: '2.5rem',
+    marginBottom: '15px',
+    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)',
+  };
+
+  const paragraphStyle = {
+    color: '#333333',
+    fontSize: '1.2rem',
+    lineHeight: '1.6',
+  };
+
   return (
-    <div className="home">
-      <h1>Welcome to ReactApp</h1>
-      <p>This is the home page content.</p>
+    <div className="home" style={containerStyle}>
+      <h1 style={headingStyle}>Welcome to 3-D model World</h1>
+      <p style={paragraphStyle}>Here you can see various 3-D models</p>
+      
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "./Navbar.css"
+import './Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/3dmodel" className="nav-link">
-            3D Model
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/services" className="nav-link">
-            Services
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/contact" className="nav-link">
-            Contact
-          </Link>
-        </li>
-     
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          TechFour
+        </Link>
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <Link to="/" className="nav-links">
+              Home
+            </Link>
+            <Link to="/3dmodel" className="nav-links">
+              3D model
+            </Link>
+          </li>
+          {/* Add more menu items here */}
+        </ul>
+      </div>
     </nav>
   );
 }
