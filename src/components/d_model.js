f2c63342d6dc42594a5b61cda24ae6e356b7a0c3
Jhamtanidev <jhamtanidev2004@gmail.com>
2023-08-26 12:49:27 +0530

    test

--- src/components/d_model.js
+++ src/components/d_model.js
@@ -2,8 +2,7 @@ import React, { useEffect } from 'react';
 import * as THREE from 'three';
 
 function ThreeJSBridge(e) {
-    
-  useEffect(() => {
+    useEffect(() => {
     // Initialize scene, camera, and renderer
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
